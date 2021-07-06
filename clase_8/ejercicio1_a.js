e2b1408f8c685ef83f53bd54845430488640cb1f
ErikVerna <vernaerik@gmail.com>
2021-07-05 23:57:02 -0300

    Ejercicios

---
+++ clase_8/ejercicio1_a.js
@@ -0,0 +1,6 @@
+// Reescribir la siguiente función tal que retorne una promesa que se cumpla luego 
+// de 3 segundos (usar setTimeout), devolviendo el mismo resultado que la función dada.
+
+function dividirNumeros(dividendo, divisor) {
+    return dividendo / divisor;
+}
